41c95f10f20804c92262a9fa2752dd4883b323a2
AESHA GABHAWALA <aeshagabha@gmail.com>
2020-04-21 22:15:54 -0700

    DOc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,30 @@
+class DocsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create # does not have view itself
+    end
+
+    def edit
+    end
+
+    def update # does not have view itself
+    end
+
+    def destroy
+    end
+
+    private
+        def find_doc
+        end
+
+        def doc_params
+        end
+end
