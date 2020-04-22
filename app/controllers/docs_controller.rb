69e610cf11d888b056ff853fa09db2d2ad2debe4
AESHA GABHAWALA <aeshagabha@gmail.com>
2020-04-22 00:56:34 -0700

    Associations between User and Docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -10,11 +10,13 @@ class DocsController < ApplicationController
     end
 
     def new # responsible for view file
-        @doc=Doc.new
+        #@doc=Doc.new # before associations
+        @doc = current_user.docs.build
     end
 
     def create # does not have view itself
-        @doc=Doc.new(doc_params)
+        #@doc=Doc.new(doc_params) # before associations
+        @doc = current_user.docs.build(doc_params)
 
         if @doc.save
             redirect_to @doc
